3aa18dcc194866a360243865d8f85f9d0dfeef77
adrisj7 <adrisj7@gmail.com>
2017-02-14 08:11:48 -0500

    Add MergeSortTester Skeleton

---
+++ MergeSortTester.java
@@ -0,0 +1,44 @@
+/*****************
+ * Adris Jautakas, Dima Hvirtsman, Ibnul Jahan
+ * APCS pd2
+ * HW#07 : What Does the Data Say?
+ * 2017-02-15
+ ******************/
+
+
+/*======================================
+  class MergeSortTester
+
+  ALGORITHM:
+  <INSERT YOUR DISTILLATION OF ALGO HERE>
+
+  BIG-OH CLASSIFICATION OF ALGORITHM:
+  <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
+
+  Mean execution times for dataset of size n:
+  Batch size: <# of times each dataset size was run>
+  n=1       time: 
+  n=10      time: 
+  n=100     time: 
+  ...
+  n=<huge>  time: 
+
+  ANALYSIS:
+  <INSERT YOUR RESULTS ANALYSIS HERE>
+  ======================================*/
+
+public class MergeSortTester 
+{
+
+    /******************************
+     * execution time analysis 
+     * <INSERT YOUR DESCRIPTION HERE OF 
+     *  YOUR APPARATUS FOR GENERATING EXECUTION 
+     *  TIME DATA...>
+     ******************************/
+    public static void main( String[] args ) 
+    {
+
+    }//end main
+
+}//end class
